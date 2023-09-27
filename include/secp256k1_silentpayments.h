5b4c8500ef0c779813c491447f3ce3925e873187
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2023-09-28 00:30:20 +0200

    silentpayments: implement output pubkey creation (both sender and receiver)

--- include/secp256k1_silentpayments.h
+++ include/secp256k1_silentpayments.h
@@ -194,6 +194,42 @@ SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_silentpayments_create_a
     const unsigned char *label_tweak32
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4);
 
+/** Create Silent Payment output public key (both for sender and receiver).
+ *
+ *  Given a shared_secret, a recipient's spend public key B_spend, and
+ *  an output counter k, calculate the corresponding output public key:
+ *
+ *  t_k = hash(shared_secret || ser_32(k))
+ *  P_output_xonly = B_spend + t_k * G
+ *
+ *  The output in x-only is always returned, as it is relevant both for
+ *  creating outputs and scanning. The plain output public key and the
+ *  tweak t_k can be returned optionally; both are only relevant for
+ *  the receiver side.
+ *
+ *  Returns: 1 if output creation was successful. 0 if an error occured.
+ *  Args:               ctx: pointer to a context object
+ *  Out:     P_output_xonly: pointer to the resulting output x-only pubkey
+ *                 P_output: pointer to the resulting output pubkey (can be NULL
+ *                           if the caller doesn't need the plain output pubkey)
+ *                      t_k: pointer to the resulting tweak t_k (can be NULL
+ *                           if the caller doesn't need the tweak)
+ *  In:     shared_secret33: shared secret, derived from either sender's
+ *                           or receiver's perspective with routines from above
+ *    receiver_spend_pubkey: pointer to the receiver's spend pubkey
+ *                        k: output counter (usually set to 0, should be increased for
+ *                           every additional output to the same recipient)
+ */
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_silentpayments_create_output_pubkey(
+    const secp256k1_context *ctx,
+    secp256k1_xonly_pubkey *P_output_xonly,
+    secp256k1_pubkey *P_output,
+    unsigned char *t_k,
+    const unsigned char *shared_secret33,
+    const secp256k1_pubkey *receiver_spend_pubkey,
+    unsigned int k
+) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(5) SECP256K1_ARG_NONNULL(6);
+
 #ifdef __cplusplus
 }
 #endif
